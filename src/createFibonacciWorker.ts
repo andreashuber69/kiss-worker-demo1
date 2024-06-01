e37bb7cbedfec62a389de64605639aee70bd9f47
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-06-01 10:09:56 +0200

    refactor: adapt to current example code

--- src/createFibonacciWorker.ts
+++ src/createFibonacciWorker.ts
@@ -1,13 +1,13 @@
-import { implementFunctionWorker } from "kiss-worker";
+import { implementFunctionWorker, Worker } from "kiss-worker";
 
 // The function we want to execute on a worker thread
 const fibonacci = (n: number): number =>
     ((n < 2) ? Math.floor(n) : fibonacci(n - 1) + fibonacci(n - 2));
 
 export const createFibonacciWorker = implementFunctionWorker(
-    // A function that creates a web worker running this script
+    // A function that creates a worker running this script
     () => new Worker(
-        new URL("createFibonacciWorker.js", import.meta.url),
+        new URL("createFibonacciWorker.ts", import.meta.url),
         { type: "module" },
     ),
     fibonacci,
